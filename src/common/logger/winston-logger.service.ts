cae40d5e64d30a32756f736d5d56f85b8882041f
Buğrahan ÖZTÜRK <bugrahanozturk@192.168.1.9>
2025-01-31 23:56:08 +0300

    feat: add error handling and logger classes

---
+++ src/common/logger/winston-logger.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, LoggerService } from '@nestjs/common';
+import * as winston from 'winston';
+
+@Injectable()
+export class WinstonLogger implements LoggerService {
+  private readonly logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({
+        filename: 'logs/error.log',
+        level: 'error',
+      }),
+    ],
+  });
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { WinstonLogger } from './common/logger/winston-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet()); // Set up security-related HTTP headers
+  app.useGlobalFilters(new HttpExceptionFilter()); // Apply the global exception filter
+  const logger = new WinstonLogger();
+  app.useLogger(logger); // Use the WinstonLogger class as the global logger
   await app.listen(3000);
 }
 bootstrap();
