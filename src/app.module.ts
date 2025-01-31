e84b1fe4290a6a7ab804ccf29cb0b099383ea123
Buğrahan ÖZTÜRK <bugrahanozturk@192.168.1.9>
2025-01-31 11:58:59 +0300

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AlertsModule } from './alerts/alerts.module';
+import { JobsModule } from './jobs/jobs.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, AlertsModule, JobsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
